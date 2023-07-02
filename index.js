8c6d05b0bd76cca17b98488329dcdfcfb30c202f
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-07-02 13:47:38 +0300

    added list of movies

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 // fetch data from the db.json local server
-fetch("db.json")
+fetch("http://localhost:3000/films")
 .then(response => response.json())
 .then( data => {
     console.log (data);
-} )
+
+// display the data on the console
+const movies = document.getElementById("movies")
+data.forEach((films) => {
+    const movieList = document.createElement("li")
+    movieList.textContent = films.title
+    movies.appendChild(movieList)
+});
+})
 .catch(error => {
     console.log('Error:', error);
   });
-// display the data on the console
-const movies = document.getElementById("movies")
-movies.innerHTML = "";
-
-for( let films = 1; films <= 15; films++){
-    const movieList = document.createElement("div")
-    // films.appendChild(movieList)
-}
-

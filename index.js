a373b9b9d16896370b701a636f1d81d8c16825da
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-07-02 23:01:53 +0300

    displayed movie details

--- index.js
+++ index.js
@@ -10,8 +10,37 @@ data.forEach((films) => {
     const movieList = document.createElement("li")
     movieList.textContent = films.title
     movies.appendChild(movieList)
-});
+
+    // displaying details of the movies
+const displayFilms = document.getElementById("display")
+const display1 = document.createElement("Div")
+display1.innerHTML =`
+<div id = "title">
+    <h1>
+        ${films.title}
+    </h1>
+</div>
+<p>${films.description}</p>
+<img src = "${films.poster}" alt = "poster" id= "poster">
+<div id= "buttons">
+    <div id= "runtime">
+        ${films.runtime}
+    </div>
+    <div id = "showtime">
+        ${films.showtime}
+    </div>
+    <div id = "capacity">
+        ${films.capacity}
+    </div>
+    <div = "tickets_sold">
+        ${films.tickets_sold}
+    </div>
+</div>
+<button id = "buy"> Buy Ticket </button>
+`
+displayFilms.appendChild(display1)
 })
-.catch(error => {
+})
+ .catch(error => {
     console.log('Error:', error);
   });
